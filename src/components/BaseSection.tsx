ccf9940b924f7c10d63ec0134358509ab398cd30
fl0gero <amir.april15@gmail.com>
2023-09-10 23:59:40 +0200

    Fixed some bugs

--- src/components/BaseSection.tsx
+++ src/components/BaseSection.tsx
@@ -19,7 +19,11 @@ export const BaseSection = ({
     <section className="relative col-span-1 row-span-1 rounded-lg bg-white bg-opacity-10 p-3">
       <h3 className="w-fit opacity-50">{title}</h3>
       <h1 className="mt-4 w-fit text-5xl">
-        {value ? (typeof value == "string" ? value : Math.round(value)) : null}
+        {value !== undefined
+          ? typeof value == "string"
+            ? value
+            : Math.round(value)
+          : null}
         {value ? measurement : null}
       </h1>
       {description ? <h2 className="w-fit">{description}</h2> : null}
